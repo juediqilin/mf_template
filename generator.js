903e442a74e02aa9cff81a8a527efff412b69db5
范远麟 <fanyuanlin@cnliqi.com>
2022-03-31 11:07:57 +0800

    11

--- generator.js
+++ generator.js
@@ -38,40 +38,92 @@ module.exports = (api, options, rootOptions) => {
 
   // 安装一些基础公共库
   api.extendPackage({
-    dependencies: {
-      "core-js": "^3.6.4",
-      "terser-webpack-plugin": "^2.3.5",
-      "vue": "^2.6.11",
-      "vue-router": "^3.1.5",
-      "vuex": "^3.1.2"
+    "name": "mf_template",
+    "version": "0.1.0",
+    "private": true,
+    "scripts": {
+      "serve": "vite",
+      "dev": "vue-cli-service serve",
+      "build": "vue-cli-service build",
+      "test:unit": "vue-cli-service test:unit",
+      "lint": "vue-cli-service lint"
     },
-    devDependencies: {
-      "@vue/cli-plugin-babel": "~4.2.0",
-      "@vue/cli-plugin-eslint": "~4.2.0",
-      "@vue/cli-plugin-router": "~4.2.0",
-      "@vue/cli-plugin-vuex": "~4.2.0",
-      "@vue/cli-service": "~4.2.0",
-      "@vue/eslint-config-prettier": "^6.0.0",
-      "babel-eslint": "^10.0.3",
-      "eslint": "^6.7.2",
-      "eslint-plugin-prettier": "^3.1.1",
-      "eslint-plugin-vue": "^6.1.2",
-      "less": "^3.0.4",
-      "less-loader": "^5.0.0",
-      "lint-staged": "^9.5.0",
-      "prettier": "^1.19.1",
-      "vue-template-compiler": "^2.6.11"
+    "dependencies": {
+      "axios": "^0.26.1",
+      "core-js": "^3.8.3",
+      "dayjs": "^1.11.0",
+      "element-plus": "^2.1.7",
+      "html2canvas": "^1.4.1",
+      "qrcode": "^1.5.0",
+      "reset.scss": "^1.0.0",
+      "vue": "^3.2.13",
+      "vue-i18n": "^9.2.0-beta.33",
+      "vue-router": "^4.0.3",
+      "vuex": "^4.0.0"
+    },
+    "devDependencies": {
+      "@babel/core": "^7.12.16",
+      "@babel/eslint-parser": "^7.12.16",
+      "@intlify/vite-plugin-vue-i18n": "^3.3.1",
+      "@vitejs/plugin-vue": "^2.2.4",
+      "@vue/cli-plugin-babel": "~5.0.0",
+      "@vue/cli-plugin-eslint": "~5.0.0",
+      "@vue/cli-plugin-router": "~5.0.0",
+      "@vue/cli-plugin-unit-jest": "~5.0.0",
+      "@vue/cli-plugin-vuex": "~5.0.0",
+      "@vue/cli-service": "~5.0.0",
+      "@vue/eslint-config-standard": "^6.1.0",
+      "@vue/test-utils": "^2.0.0-0",
+      "@vue/vue3-jest": "^27.0.0-alpha.1",
+      "babel-jest": "^27.0.6",
+      "eslint": "^7.32.0",
+      "eslint-plugin-import": "^2.25.3",
+      "eslint-plugin-node": "^11.1.0",
+      "eslint-plugin-promise": "^5.1.0",
+      "eslint-plugin-vue": "^8.0.3",
+      "jest": "^27.0.5",
+      "sass": "^1.32.7",
+      "sass-loader": "^12.0.0",
+      "unplugin-auto-import": "0.5.3",
+      "vite": "2.6.14",
+      "vite-plugin-compression": "0.3.6",
+      "vite-plugin-svg-icons": "1.0.5",
+      "vite-plugin-vue-setup-extend": "0.1.0"
+    },
+    "eslintConfig": {
+      "root": true,
+      "env": {
+        "node": true
+      },
+      "extends": [
+        "plugin:vue/vue3-essential",
+        "@vue/standard"
+      ],
+      "parserOptions": {
+        "parser": "@babel/eslint-parser"
+      },
+      "rules": {},
+      "overrides": [
+        {
+          "files": [
+            "**/__tests__/*.{j,t}s?(x)",
+            "**/tests/unit/**/*.spec.{j,t}s?(x)"
+          ],
+          "env": {
+            "jest": true
+          }
+        }
+      ]
+    },
+    "browserslist": [
+      "> 1%",
+      "last 2 versions",
+      "not dead",
+      "not ie 11"
+    ],
+    "jest": {
+      "preset": "@vue/cli-plugin-unit-jest"
     }
   });
-  // 删除 vue-cli3 默认目录
-  api.render(files => {
-    Object.keys(files)
-      .filter(path => path.startsWith('src/') || path.startsWith('public/'))
-      .forEach(path => delete files[path])
-    console.log(Object.keys(files))
-  })
-  api.render('../template');
-  api.onCreateComplete(() => {
-    process.env.VUE_CLI_SKIP_WRITE = true;
-  });
+  api.render('./template');
 };
